1d3aed70449e55d2c0982b948f09689473a3ba62
just_east3656 <80552756+Lingly70@users.noreply.github.com>
2024-05-14 22:14:48 -0400

    Update RSAtools.c
    added gcd and coprime functions.

--- RSAtools.c
+++ RSAtools.c
@@ -5,7 +5,9 @@
 //FUNCTION PROTOTYPE DECLARATION START
 char* encrypted(const char buffer[], int exp);
 char exponent(int base, int exp);
-bool isPrime(int n);
+bool is_prime(int n);
+int gcd(int a, int b);
+bool is_coprime(int num1, int num2);
 //FUNCTION PROTOTYPE DECLARATION END
 
 
@@ -21,15 +23,15 @@ char* encrypted(const char buffer[], int exp) {
 }
 
 char exponent(char base, int exp){
-  int current_letter = (int)base; 
+  	int current_letter = (int)base; 
 
 	if(exp==0){	//verifying base case
 		return 1;
 	}else{
-		return (char)(current_letter*exponent(base, exp-1)); //recursion called.
+		return (char)(current_letter*(int)exponent(base, exp-1)); //recursion called.
 }
 
-bool isPrime(int n) {
+bool is_prime(int n) {
     if (n <= 1) return false;
     if (n == 2) return true;  
     if (n % 2 == 0) return false;  // checking if 0, 1, or an even number.
@@ -37,11 +39,21 @@ bool isPrime(int n) {
     for (int i = 3; i <= sqrt(n); i += 2) {
         if (n % i == 0) return false;  //Trial division method applied in the forloop.
     }
-
-
-  //FUNCTION END.
-	
     return true;  //if all previous tests are false then the int provided is true for being prime. 
+}
+
+
+int gcd(int a, int b) {
+    while (b != 0) {
+        int t = b;
+        b = a % b;
+        a = t;
+    }
+    return a;
+}
+
+bool is_coprime(int num1, int num2){
+	return (gcd(num1, num2)==1);
+}
 
 	
-}
