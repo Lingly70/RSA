9c7535635fea9315b5ca301f6f948bf7366b354f
manshen <80552756+Lingly70@users.noreply.github.com>
2024-05-16 19:29:31 -0400

    Create RSAtools.h

---
+++ RSAtools.h
@@ -0,0 +1,13 @@
+#ifndef RSATOOLS_H
+#define RSATOOLS_H
+
+
+char* encrypt(const char buffer[], int exp);
+char exponent(char base, int exp);
+int gcd(int a, int b);
+int is_coprime(int num1, int num2);
+int is_prime(int n);
+int mod_inverse(int e, int phi);
+
+
+#endif
