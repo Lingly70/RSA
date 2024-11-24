bc6215abe58e6129c4098564c80eeefce77998b0
manshen <80552756+Lingly70@users.noreply.github.com>
2024-11-24 12:23:10 -0500

    Update RSAtools.h

--- RSAtools.h
+++ RSAtools.h
@@ -8,12 +8,13 @@
 #include <math.h>
 
 
-char* encrypt(const char buffer[], int exp);
+char* encrypt(const char buffer[], int e, int n);
+char* decrypt(const char buffer[], int d, int n);
+
 char exponent(char base, int exp);
 int gcd(int a, int b);
 int is_coprime(int num1, int num2);
 int is_prime(int n);
 int mod_inverse(int e, int phi);
-
-
+int mod_exp(int base, int exp, int mod);
 #endif
